2905739cc51a3c52684535e4984ecee96d12a439
az-ien <azlan.malik@iengineering.com>
2025-12-30 17:10:46 +0500

    may be ?? not understood why

--- tests/popup.spec.ts
+++ tests/popup.spec.ts
@@ -8,6 +8,8 @@ await page.goto("https://testautomationpractice.blogspot.com/");
 
 // same concept but event is now popup not tab window
 await Promise.all([page.waitForEvent('popup'),page.locator('#PopUp').click()]) // array created here 
+await Promise.all([page.waitForEvent('popup'),page.locator('#PopUp').click()]) // but why ??? maybe because it opens an unreliable popup window
+
 const allPopuwindows = context.pages() // array of pages returned 
 console.log("popup windows",allPopuwindows.length) //3 pages
 
@@ -16,10 +18,9 @@ console.log("popup windows",allPopuwindows.length) //3 pages
 
 
 
-
-// console.log("parent ", allPopuwindows[0].url())
-// console.log("child ", allPopuwindows[1].url())
-// console.log("child ", allPopuwindows[2].url())
+console.log("parent ", allPopuwindows[0].url())
+console.log("child ", allPopuwindows[1].url())
+console.log("child ", allPopuwindows[2].url())
 
 // // for (const popw of allPopuwindows)
 // // {
