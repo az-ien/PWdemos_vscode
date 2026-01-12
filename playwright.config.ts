6fb7957b85bfd7bc3eba8231a8590234b79fe0cd
az-ien <azlan.malik@iengineering.com>
2026-01-12 15:21:26 +0500

    allure reports added to the tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // need to install npm allure reports then allure command line then run them using the command line for allure reports npx allure serve allure-results
+  reporter:[ ['html'],['allure-playwright'], ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
