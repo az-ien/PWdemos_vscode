95b3a2d54f3b2c52c8ebfeba46c8b0cb06e65eac
az-ien <azlan.malik@iengineering.com>
2026-03-24 12:33:23 +0500

    removed the process configs to see if error is rmeoved

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,9 +33,9 @@ export default defineConfig({
   //forbidOnly: !!process.env.CI,
   forbidOnly: false,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // need to install npm allure reports then allure command line then run them using the command line for allure reports npx allure serve allure-results
   reporter:[ ['html'],['allure-playwright'], ],
